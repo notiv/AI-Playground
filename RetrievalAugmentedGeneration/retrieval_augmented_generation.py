5135ab325e45567919424d8eb516ac85ef7e2b42
Viton Vitanis <notiv@vitons-macbook-pro-m2.home>
2023-12-18 20:46:46 +0100

    Create document from news articles and query the documents

--- RetrievalAugmentedGeneration/retrieval_augmented_generation.py
+++ RetrievalAugmentedGeneration/retrieval_augmented_generation.py
@@ -3,16 +3,25 @@
 import os
 
 import hvac
+
+from datetime import date, timedelta
+
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA, create_qa_with_sources_chain
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import StdOutCallbackHandler
 
-# Retrieve the openai_test_token from Vault
+from langchain.docstore.document import Document
+
+from newsapi import NewsApiClient
+#%% 
+# Retrieve the openai_test_token and the newsapi_token from Vault
 client = hvac.Client(url='http://localhost:8200')
 client.token = os.environ['VAULT_TOKEN']
 
@@ -23,10 +32,17 @@ if not client.is_authenticated():
 
 try:
     openai_token = client.secrets.kv.v2.read_secret(path='openai', mount_point='kv')['data']['data']['openai_test_token']
-     #.v2.read_secret(path='secrets/api_readonly/openai_test_token')
 except hvac.exceptions.InvalidPath:
-    print('The secret path is invalid.')
+    print('The openai secret path is invalid.')
     exit(1)
+
+#%%
+try:
+    newsapi_token = client.secrets.kv.v2.read_secret(path='news_api', mount_point='kv')['data']['data']['news_api_key']
+except hvac.exceptions.InvalidPath:
+    print('The openai secret path is invalid.')
+    exit(1)
+
 
 
 file_path = './ESLII_print12_toc.pdf'
@@ -70,9 +86,65 @@ chain = RetrievalQA.from_chain_type(
     verbose=True
 )
 
-handler = StdOutCallbackHandlerq()
+handler = StdOutCallbackHandler()
 
 chain.run(
     'What is machine learning?',
     callbacks=[handler]
 )
+# %%
+newsapi = NewsApiClient(api_key=newsapi_token)
+
+today = date.today()
+last_week = today - timedelta(days=7)
+
+## Get the news on 'Artificial Intelligence' from last week
+latest_news = newsapi.get_everything(
+    q='artificial intelligence',
+    from_param=last_week.strftime('%Y-%m-%d'),
+    to=today.strftime('%Y-%m-%d'),
+    sort_by='relevancy',
+    language='en'
+)
+
+# %%
+## Create documents 
+docs = [
+    Document(
+        page_content=article['title'] + '\n\n' + article['description'] if article['description'] else '',
+        metadata = {
+            'source': article['url']
+        }
+    ) for article in latest_news['articles']
+]
+# %%
+## Create a chain that provides the sources with the answers
+qa_chain = create_qa_with_sources_chain(llm)
+
+doc_prompt = PromptTemplate(
+    template='Content: {page_content}\nSource: {source}',
+    input_variables=['page_content', 'source'],
+)
+
+final_qa_chain = StuffDocumentsChain(
+    llm_chain=qa_chain,
+    document_variable_name='context',
+    document_prompt=doc_prompt,
+)
+
+index = FAISS.from_documents(docs, embedding=embeddings)
+
+chain = RetrievalQA(
+    retriever=index.as_retriever(),
+    combine_documents_chain=final_qa_chain
+)
+
+# %%
+## Ask a question
+question = """
+What is the most important news about artificial intelligence from last week?
+"""
+# %%
+answer = chain.run(question)
+print(answer)
+# %%
